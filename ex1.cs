2990db8205e18d78576fa80f580f6d2b17a6bb16
boris-1234 <60132750+boris-1234@users.noreply.github.com>
2020-03-24 16:33:39 +0200

    Add files via upload

---
+++ ex1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace class_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+         
+
+            //קליטת מספר ראשון
+            Console.WriteLine("enter please the Array Length :");
+            // length of array :
+            int lengthofArray = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter number to multiply :");
+            // number to multiply each cell in the array :
+            int numToMul = Convert.ToInt32(Console.ReadLine());
+
+            // initialize array :
+
+            int[] arr = new int[lengthofArray];
+
+            Console.WriteLine($"Num of elements  : {lengthofArray} \n,number to multiply : {numToMul}\n");
+            for (int i=0; i<arr.Length;i++)
+            {
+                arr[i] = i * numToMul;
+                Console.Write(arr[i] + ",");
+            }
+
+        }
+    }
+}
